1150aa391155c1c4fd9f06a4087bd24d80290234
hiteshnerdcode <hiteshandmatha@gmail.com>
2026-03-16 15:40:08 +0530

    Akshata Added UseCase3PalindromeChecker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,21 +1,25 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    static void main(String[] args) {
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
 
-        // Hardcoded string
-        String word = "madam";
-
-        // Reverse the string
+        // Step 1: Reverse string using for loop
         String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);  // String Concatenation
         }
 
-        // Check palindrome using if-else
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome");
+        // Step 2: Compare original and reversed using equals()
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome");
         } else {
-            System.out.println(word + " is NOT a Palindrome");
+            System.out.println(original + " is NOT a Palindrome");
         }
+
+        sc.close();
     }
 }
